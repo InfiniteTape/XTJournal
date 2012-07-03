070e34505c4204d24a0847e1ef35e22b8a5754a4
Brian Rogers <brogers@beckman.com>
2012-07-03 16:56:50 -0400

    Added prototype cell and backing class for History view.

---
+++ XTJournal/XTJHistoryViewController.h
@@ -0,0 +1,15 @@
+//
+//  XTJHistoryViewController.h
+//  XTJournal
+//
+//  Created by Rogers, Brian A on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XTJHistoryViewController : UIViewController
+
+@property (strong, nonatomic) NSMutableDictionary* dataStore;
+
+@end
---
+++ XTJournal/XTJHistoryViewController.m
@@ -0,0 +1,45 @@
+//
+//  XTJHistoryViewController.m
+//  XTJournal
+//
+//  Created by Rogers, Brian A on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XTJHistoryViewController.h"
+
+@interface XTJHistoryViewController ()
+
+@end
+
+@implementation XTJHistoryViewController
+
+@synthesize dataStore;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
